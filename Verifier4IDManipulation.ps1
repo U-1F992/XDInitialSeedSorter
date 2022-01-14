bf09fd42a1a3fa8d1b119a3e04974da06d85b860
Kôtaro Mukai <mukai.k1011k@outlook.jp>
2022-01-14 16:49:47 +0900

    encoding

--- Verifier4IDManipulation.ps1
+++ Verifier4IDManipulation.ps1
@@ -2,30 +2,41 @@
 
 using assembly PokemonCoRNGLibrary.dll
 
+$defaultOutputEncoding = $OutputEncoding
+$defaultConsoleInputEncoding = [Console]::InputEncoding
+$defaultConsoleOutputEncoding = [Console]::OutputEncoding
+$OutputEncoding = [Console]::InputEncoding = [Console]::OutputEncoding = New-Object System.Text.UTF8Encoding
+
 [UInt32] $target = [UInt32] 0x514fa562L
 [UInt32] $seed = 0
 
 [long] $count = 0
 
-do {
+try {
+    do {
 
-    $count++
-    Write-Host "--- Attempt: $count ---"
-    Write-Host ""
+        $count++
+        Write-Host "--- Attempt: $count ---"
+        Write-Host ""
 
-    .\XDInitialSeedSorter.exe | Tee-Object result.txt
+        .\XDInitialSeedSorter.exe | Tee-Object result.txt
 
-    [string] $result = (Get-Content result.txt)
-    [string[]] $seeds = [RegEx]::Matches($result, "Current seed: [0123456789abcdefABCDEF]{0,8}")
+        [string] $result = (Get-Content result.txt)
+        [string[]] $seeds = [RegEx]::Matches($result, "Current seed: [0123456789abcdefABCDEF]{0,8}")
 
-    $seed = [System.Convert]::ToUInt32($seeds[$seeds.Length - 1].Split(" ")[2], 16)
-    Write-Host "Current seed: $([System.Convert]::ToString($seed, 16))"
-    Write-Host ""
+        $seed = [System.Convert]::ToUInt32($seeds[$seeds.Length - 1].Split(" ")[2], 16)
+        Write-Host "Current seed: $([System.Convert]::ToString($seed, 16))"
+        Write-Host ""
 
-} while (
-    ![System.Linq.Enumerable]::Take(
-        [System.Linq.Enumerable]::Skip(
-            [PokemonCoRNGLibrary.SeedEnumerator]::EnumerateSeedAtNamingScreen($seed),
-        116 + 607),
-    1000000).ToArray().Contains($target)
-)
+    } while (
+        ![System.Linq.Enumerable]::Take(
+            [System.Linq.Enumerable]::Skip(
+                [PokemonCoRNGLibrary.SeedEnumerator]::EnumerateSeedAtNamingScreen($seed),
+            116 + 607),
+        1000000).ToArray().Contains($target)
+    )
+} finally {
+    $OutputEncoding = $defaultOutputEncoding
+    [Console]::InputEncoding = $defaultConsoleInputEncoding
+    [Console]::OutputEncoding = $defaultConsoleOutputEncoding
+}
