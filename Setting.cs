74ed929cca6a143c4ef07ef115dab1de47b97a3e
Kôtaro Mukai <mukai.k1011k@outlook.jp>
2022-01-24 16:25:04 +0900

    namespace

--- Setting.cs
+++ Setting.cs
@@ -1,62 +1,66 @@
 ﻿#pragma warning disable CS8618
-public class Setting
+
+namespace PokemonXD
 {
-    public PokemonXD pokemonXD { get; set; }
-    public Binaries binaries { get; set; }
-    public Devices devices { get; set; }
-    public Dictionary<string, Rect> crops { get; set; }
-
-    public class PokemonXD
+    public class Setting
     {
-        public List<UInt32> targets { get; set; }
-        public int leftover { get; set; }
-        public double consumptionPerSec { get; set; }
-    }
+        public PokemonXD pokemonXD { get; set; }
+        public Binaries binaries { get; set; }
+        public Devices devices { get; set; }
+        public Dictionary<string, Rect> crops { get; set; }
 
-    public class Binaries
-    {
-        public string tesseractOCR { get; set; }
-        public string xdDatabase { get; set; }
-    }
-
-    public class Devices
-    {
-        public Controller controller { get; set; }
-        public Capture capture { get; set; }
-
-        public class Controller
+        public class PokemonXD
         {
-            public string port { get; set; }
-            public int delayAfterReset { get; set; }
+            public List<UInt32> targets { get; set; }
+            public int leftover { get; set; }
+            public double consumptionPerSec { get; set; }
         }
 
-        public class Capture
+        public class Binaries
         {
-            public int index { get; set; }
+            public string tesseractOCR { get; set; }
+            public string xdDatabase { get; set; }
+        }
+
+        public class Devices
+        {
+            public Controller controller { get; set; }
+            public Capture capture { get; set; }
+
+            public class Controller
+            {
+                public string port { get; set; }
+                public int delayAfterReset { get; set; }
+            }
+
+            public class Capture
+            {
+                public int index { get; set; }
+                public int width { get; set; }
+                public int height { get; set; }
+                public bool showImage { get; set; }
+            }
+        }
+
+        public class Crops
+        {
+            public Rect player { get; set; }
+            public Rect com { get; set; }
+            public Rect hp_1 { get; set; }
+            public Rect hp_2 { get; set; }
+            public Rect hp_3 { get; set; }
+            public Rect hp_4 { get; set; }
+
+            
+        }
+
+        public class Rect
+        {
             public int width { get; set; }
             public int height { get; set; }
-            public bool showImage { get; set; }
+            public int x { get; set; }
+            public int y { get; set; }
         }
-    }
-
-    public class Crops
-    {
-        public Rect player { get; set; }
-        public Rect com { get; set; }
-        public Rect hp_1 { get; set; }
-        public Rect hp_2 { get; set; }
-        public Rect hp_3 { get; set; }
-        public Rect hp_4 { get; set; }
-
-        
-    }
-
-    public class Rect
-    {
-        public int width { get; set; }
-        public int height { get; set; }
-        public int x { get; set; }
-        public int y { get; set; }
     }
 }
 #pragma warning restore CS8618
